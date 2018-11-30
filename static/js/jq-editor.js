3295418dedf520d7b7a7df67b0613775eebcb9dc
guojianxiang <jianxiang.guo@xuebangsoft.me>
2018-11-30 16:45:05 +0800

    feat: 光标位置可以追踪到图片视频的下一行

--- static/js/jq-editor.js
+++ static/js/jq-editor.js
@@ -2,11 +2,11 @@ var contentEditor = new Eleditor({
 	el: '#contentEditor',
 	/*初始化完成钩子*/
 });
+$('.textarea').focus();
 $('.Eleditor-textStyle-item-upImg').on('click', function(e) {
 	if (window.FastClick) {
 		$('.Eleditor-input-file-image').triggerFastClick();
 	} else {
-		console.log('cc')
 		$('.Eleditor-input-file-image').click();
 	}
 });
@@ -24,9 +24,14 @@ $('.Eleditor-input-file-image').on('change', function(e) {
 		selection.removeAllRanges()
 		selection.addRange(window.lastEditRange)
 	}
-	pasteHtmlAtCaret('<img src="https://pingtai-uat.oss-cn-shenzhen.aliyuncs.com/29d26e78-f7c7-45a6-9042-1e55f5869eac.jpg"/>')
-	// pasteHtmlAtCaret('<video src="https://pingtai-uat.oss-cn-shenzhen.aliyuncs.com/29d26e78-f7c7-45a6-9042-1e55f5869eac.jpg"><video/>')
-	window.lastEditRange = selection.getRangeAt(0)
+    pasteHtmlAtCaret('\
+    <div>\
+    <img src="https://pingtai-uat.oss-cn-shenzhen.aliyuncs.com/29d26e78-f7c7-45a6-9042-1e55f5869eac.jpg"/>\
+    </div>\
+    ');
+    window.lastEditRange = selection.getRangeAt(0);
+    $('.textarea').focus();
+    
 });
 $('.Eleditor-input-file-video').on('change', function(e) {
 	$('.textarea').focus();
@@ -35,8 +40,13 @@ $('.Eleditor-input-file-video').on('change', function(e) {
 		selection.removeAllRanges()
 		selection.addRange(window.lastEditRange)
 	}
-	pasteHtmlAtCaret('<video controls="controls" poster="https://oss-uat.xuebangsoft.net/814c37d3-fb33-4cc3-b395-a9f2f88ecd42.mp4?spx&amp;x-oss-process=video/snapshot,t_100,f_jpg" src="https://oss-uat.xuebangsoft.net/814c37d3-fb33-4cc3-b395-a9f2f88ecd42.mp4"></video>')
-	window.lastEditRange = selection.getRangeAt(0)
+    pasteHtmlAtCaret('\
+    <div>\
+    <video controls="controls" poster="https://oss-uat.xuebangsoft.net/814c37d3-fb33-4cc3-b395-a9f2f88ecd42.mp4?spx&amp;x-oss-process=video/snapshot,t_100,f_jpg" src="https://oss-uat.xuebangsoft.net/814c37d3-fb33-4cc3-b395-a9f2f88ecd42.mp4"></video>\
+    </div>\
+    ');
+    window.lastEditRange = selection.getRangeAt(0)
+    $('.textarea').focus();
 });
 $('.submit').on('click', function() {
 	var domf = $('.Eleditor-inputarea').clone(),domC = domf.find('.textarea');
